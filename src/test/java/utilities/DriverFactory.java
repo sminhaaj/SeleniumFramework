09579a2fbc38fec71d31706ead99ff1713a9c667
sminhaaj <akms.minhaaj@gmail.com>
2023-08-19 21:26:43 -0500

    Update

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -1,9 +1,15 @@
 package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.asm.MemberSubstitution;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -18,10 +24,33 @@ public class DriverFactory {
     }
 
     ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(() -> {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        return new ChromeDriver(options);
+        String environment = System.getProperty("environment") == null? "local": System.getProperty("environment");
+        String browser = System.getProperty("browser") == null? "chrome": System.getProperty("browser");
+        URL gridUrl = null;
+
+        try{
+            gridUrl = new URL(ReadConfigFiles.getPropertyValues("GridUrl"));
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(environment.equals("remote") && browser.equals("chrome")){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            return new RemoteWebDriver(gridUrl, chromeOptions);
+        } else if(environment.equals("remote") && browser.equals("firefox")){
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            return new RemoteWebDriver(gridUrl, firefoxOptions);
+        } else if(environment.equals("remote") && browser.equals("edge")){
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--remote-allow-origins=*");
+            return new RemoteWebDriver(gridUrl, edgeOptions);
+        } else{
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            return new ChromeDriver(options);
+        }
     });
 
     public WebDriver getDriver(){
